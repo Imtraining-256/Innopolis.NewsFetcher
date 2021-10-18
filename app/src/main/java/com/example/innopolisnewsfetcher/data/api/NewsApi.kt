652227e9751b377ef8fe705e6ce7dcccb539354a
atenyaeves <atenyaev@protonmail.com>
2021-10-18 22:46:09 +0700

    Added api interface

---
+++ app/src/main/java/com/example/innopolisnewsfetcher/data/api/NewsApi.kt
@@ -0,0 +1,16 @@
+package com.example.innopolisnewsfetcher.data.api
+
+import com.example.innopolisnewsfetcher.data.api.model.NewsModel
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+interface NewsApi {
+    @GET("everything")
+    @Headers("X-Api-Key: 0cf24868af49ecb5a3b6f9dc1a5bf55f")
+    suspend fun getNews(
+        @Query("q") searchQuery: String = "",
+        @Query("language") language: String = "ru",
+        @Query("country") country: String = "ru"
+    ): NewsModel
+}
