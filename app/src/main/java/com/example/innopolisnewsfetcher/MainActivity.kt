6ccb508ad3a8bba6caea8331596c665cb312dd0f
atenyaeves <atenyaev@protonmail.com>
2021-10-18 21:24:02 +0700

    Added activity and markup

--- app/src/main/java/com/example/innopolisnewsfetcher/MainActivity.kt
+++ app/src/main/java/com/example/innopolisnewsfetcher/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.innopolisnewsfetcher
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.innopolisnewsfetcher.features.main_screen.MainScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction().replace(R.id.container, MainScreenFragment())
+            .commit()
     }
 }
