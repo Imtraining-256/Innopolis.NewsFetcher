2dc236af15688dfc5ce9805f79484f0da84f8d99
atenyaeves <atenyaev@protonmail.com>
2021-10-22 23:25:03 +0700

    Added ArticleAdapter

---
+++ app/src/main/java/com/example/innopolisnewsfetcher/features/main_screen/ui/adapter/ArticlesAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.innopolisnewsfetcher.features.main_screen.ui.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.innopolisnewsfetcher.R
+import com.example.innopolisnewsfetcher.features.main_screen.domain.model.NewsDomainModel
+
+class ArticlesAdapter(private var models: List<NewsDomainModel>) :
+    RecyclerView.Adapter<ArticlesAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        val title: TextView
+
+        init {
+            this.title = view.findViewById<TextView>(R.id.textView)
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
+
+        return ViewHolder(view)
+
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.title.text = models[position].title
+    }
+
+    override fun getItemCount(): Int {
+        return models.size
+    }
+
+    fun updateArticles(newModels: List<NewsDomainModel>) {
+        models = newModels
+        notifyDataSetChanged()
+    }
+
+}
