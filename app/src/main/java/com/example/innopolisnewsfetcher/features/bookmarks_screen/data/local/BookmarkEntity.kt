aa57b34cf9a3439133752a3510fcb43992796d9e
atenyaeves <atenyaev@protonmail.com>
2021-10-25 23:14:34 +0700

    Added bookmark entity

---
+++ app/src/main/java/com/example/innopolisnewsfetcher/features/bookmarks_screen/data/local/BookmarkEntity.kt
@@ -0,0 +1,31 @@
+package com.example.innopolisnewsfetcher.features.bookmarks_screen.data.local
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.example.innopolisnewsfetcher.features.bookmarks_screen.data.local.BookmarkEntity.Companion.TABLE_NAME
+
+@Entity(tableName = TABLE_NAME)
+data class BookmarkEntity(
+    @PrimaryKey
+    @ColumnInfo(name = "url")
+    val url: String,
+    @ColumnInfo(name = "author")
+    val author: String,
+    @ColumnInfo(name = "title")
+    val title: String,
+    @ColumnInfo(name = "description")
+    val description: String,
+    @ColumnInfo(name = "urlToImage")
+    val urlToImage: String,
+    @ColumnInfo(name = "publishedAt")
+    val publishedAt: String,
+    @ColumnInfo(name = "content")
+    val content: String,
+    @ColumnInfo(name = "isBookmarked")
+    val isBookmarked: Boolean
+) {
+    companion object {
+        const val TABLE_NAME = "BOOKMARKS_TABLE"
+    }
+}
