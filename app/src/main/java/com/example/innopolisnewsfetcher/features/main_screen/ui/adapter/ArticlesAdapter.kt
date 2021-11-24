f0f12ce2edba96328bb4b048e9116475a5fba2a6
atenyaeves <atenyaev@protonmail.com>
2021-11-24 20:45:20 +0700

    Add article image

--- app/src/main/java/com/example/innopolisnewsfetcher/features/main_screen/ui/adapter/ArticlesAdapter.kt
+++ app/src/main/java/com/example/innopolisnewsfetcher/features/main_screen/ui/adapter/ArticlesAdapter.kt
@@ -3,8 +3,10 @@ package com.example.innopolisnewsfetcher.features.main_screen.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.innopolisnewsfetcher.R
 import com.example.innopolisnewsfetcher.features.main_screen.domain.model.ArticleDomainModel
 
@@ -16,6 +18,7 @@ class ArticlesAdapter(
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         val title: TextView = view.findViewById<TextView>(R.id.tvArticleTitle)
+        val img: ImageView = view.findViewById<ImageView>(R.id.tvArticleImage)
 
     }
 
@@ -24,11 +27,11 @@ class ArticlesAdapter(
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_article, parent, false)
 
         return ViewHolder(view)
-
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         holder.title.text = articleModels[position].title
+        Glide.with(holder.itemView).load(articleModels[position].urlToImage).into(holder.img)
         holder.itemView.setOnClickListener { onItemClick(articleModels[position]) }
     }
 
